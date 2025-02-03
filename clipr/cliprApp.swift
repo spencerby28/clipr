273fc355ac7e1d3dc71bfa8651b4bfdd88860f06
spencer <spenceracbyrne@gmail.com>
2025-02-03 10:24:23 -0600

    Initial Commit

---
+++ clipr/cliprApp.swift
@@ -0,0 +1,17 @@
+//
+//  cliprApp.swift
+//  clipr
+//
+//  Created by Spencer Byrne on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct cliprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
